357e48b328e094a92b3a14cbe831b560106a1f32
程序员丶阿飞 <wuningfei126@126.com>
2023-02-23 00:22:09 +0800

    create gradle project

---
+++ gradle/GradleProject/src/main/java/com/example/GradleProject/GradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.GradleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleProjectApplication.class, args);
+	}
+
+}
